e3b39374f54f95ab1dfc268d8cfce3245f873822
Alexsandre Almeida <alexsandrer@gmail.com>
2024-02-15 02:18:30 -0300

    Corrige estrutura do banco

--- db/scripts/01-createDB.sql
+++ db/scripts/01-createDB.sql
@@ -35,8 +35,9 @@ CREATE TABLE "Tasks" (
 
 CREATE TABLE "Comments" (
     "Id"  SERIAL  NOT NULL,
-    "IdTask" int   NOT NULL,
     "Text" varchar(250)   NOT NULL,
+    "IdTask" int   NOT NULL,
+    "IdUser" int   NOT NULL,
     CONSTRAINT "pk_Comments" PRIMARY KEY (
         "Id"
      )
@@ -47,8 +48,8 @@ CREATE TABLE "Logs" (
     "Field" varchar(50)   NOT NULL,
     "OldValue" varchar(250)   NOT NULL,
     "NewValue" varchar(250)   NOT NULL,
+    "NameUser" varchar(80)   NOT NULL,
     "IdTask" int   NOT NULL,
-    "IdUser" int   NOT NULL,
     CONSTRAINT "pk_Logs" PRIMARY KEY (
         "Id"
      )
@@ -63,9 +64,8 @@ REFERENCES "Projects" ("Id");
 ALTER TABLE "Comments" ADD CONSTRAINT "fk_Comments_IdTask" FOREIGN KEY("IdTask")
 REFERENCES "Tasks" ("Id");
 
-ALTER TABLE "Logs" ADD CONSTRAINT "fk_Logs_IdTask" FOREIGN KEY("IdTask")
-REFERENCES "Tasks" ("Id");
-
-ALTER TABLE "Logs" ADD CONSTRAINT "fk_Logs_IdUser" FOREIGN KEY("IdUser")
+ALTER TABLE "Comments" ADD CONSTRAINT "fk_Comments_IdUser" FOREIGN KEY("IdUser")
 REFERENCES "Users" ("Id");
 
+ALTER TABLE "Logs" ADD CONSTRAINT "fk_Logs_IdTask" FOREIGN KEY("IdTask")
+REFERENCES "Tasks" ("Id");

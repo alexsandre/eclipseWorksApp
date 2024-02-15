d57479fa8e0a2ecefa3006a30c074d8d0561c9fa
Alexsandre Almeida <alexsandrer@gmail.com>
2024-02-15 11:24:58 -0300

    Cria tratamento de erro global

---
+++ src/EclipseWorksApp.API/Config/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using EclipseWorksApp.API.Exceptions;
+using EclipseWorksApp.API.Models;
+using EclipseWorksApp.Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace EclipseWorksApp.API.Config
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (CustomHTTPException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex.Message, ex.StatusCode);
+            }
+            catch (DomainException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex.Message, StatusCodes.Status400BadRequest);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex.Message, StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, string message, int statusCode)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+
+            var response = new CustomHttpResponse(message);
+            var responseJson = JsonSerializer.Serialize(this);
+            
+            await context.Response.WriteAsync(responseJson);
+        }
+    }
+}
--- src/EclipseWorksApp.API/Program.cs
+++ src/EclipseWorksApp.API/Program.cs
@@ -32,6 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.MapControllers();
 
